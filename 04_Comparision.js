8ee067ae948001847dab36aa98bc04b9d40c1993
sakshiOfficail1992 <sakshiofficial1992@gmail.com>
2024-09-14 23:43:16 +0530

    Calculator Start

--- 04_Comparision.js
+++ 04_Comparision.js
@@ -1,4 +1,10 @@
 let name = "20"
 let age = 5
 
-console.log(name ===20);
+//console.log(name ===20);
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+
---
+++ Datatypes-summery.js
@@ -0,0 +1,31 @@
+const id = Symbol('123')
+const anotherId = Symbol('123')
+//console.log(id == anotherId);
+
+const heros = ["shaktiman", "aryaman"]
+ const age = ["20","15"]
+ //console.log(heros);
+ //console.log(age);
+ 
+ let myobj = {name : "him", age : 20}
+ //console.log(myobj);
+ 
+ 
+
+ const add = function (a,b) {return (a + b)}
+
+ let c = add (2, 3)
+ //console.log(c);
+ 
+ const subs = function (a,b) {return (a - b)}
+ let d = subs (5, 2)
+ //console.log(d);
+ 
+ const multi = function (a,b) {return (a * b)}
+ let e = multi (5, 2)
+ //console.log(e);
+
+ const divide = function (a,b) {return (a / b)}
+ let f = divide (20, 2)
+ console.log(f);
+ 
